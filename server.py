41123f0d41f05cbdae128684715851825a6934b5
Anton Agestam <git@antonagestam.se>
2018-02-15 21:42:38 +0100

    add missing await + comments

--- server.py
+++ server.py
@@ -2,11 +2,10 @@ import asyncio
 import aioredis
 import uvloop
 import sanic
-from sanic.response import file
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 app = sanic.Sanic(__name__)
-loop = asyncio.new_event_loop()
+loop = asyncio.get_event_loop()
 
 
 @app.route('/')
@@ -14,14 +13,16 @@ async def index(request):
     return await file('ws-test.html')
 
 
-async def send(ws):
+async def publish(ws):
+    """Receive messages from the WebSocket and pass them on to redis"""
     redis = await aioredis.create_redis('redis://localhost/0')
     while True:
         data = await ws.recv()
-        redis.publish_json('chan:1', data)
+        await redis.publish_json('chan:1', data)
 
 
-async def receive(ws):
+async def subscribe(ws):
+    """Receive messages from redis and pass them on to the WebSocket"""
     redis = await aioredis.create_redis('redis://localhost/0')
     channel = (await redis.subscribe('chan:1'))[0]
     while await channel.wait_message():
@@ -31,7 +32,8 @@ async def receive(ws):
 
 @app.websocket('/join')
 async def join(request, ws):
-    await asyncio.gather(send(ws), receive(ws))
+    """For every request we start a publish loop and a subscribe loop"""
+    await asyncio.gather(publish(ws), subscribe(ws))
 
 
 if __name__ == '__main__':
